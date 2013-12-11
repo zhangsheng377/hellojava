233f147932b898664786d0241228dc18599c25da
zhangshengdong29 <15948075101@qq.com>
2013-12-11 16:20:31 +0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello
+{
+    public static void main (String args[])
+    {
+        System.out.println("你好胡");
+        A a=new A();
+        a.fA();
+    }
+}
+class A
+{
+    void fA()
+    {
+        System.out.println("We are students");
+    }
+}
